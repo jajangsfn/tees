0d8676a73d985944023e394517a6c11df8049604
Sofian <sofian.informatika@gmail.com>
2020-11-12 09:02:55 +0700

    update fungsi keranjang

--- keranjang.php
+++ keranjang.php
@@ -1,71 +1,115 @@
 <?php
-
+session_start();
 include "koneksi.php";
 
 $method = $_SERVER['REQUEST_METHOD'];
 parse_str(file_get_contents("php://input"), $input);  
 
-if ($method == "GET") {
-    //get id jika ada
-    $where = isset($input['id']) ? "WHERE id=".$input['id'] : "";
-    
-    $qry = $conn->query("SELECT * FROM keranjang ".$where);
+if (isset($_SESSION['is_login'])) {
+    if ($method == "GET") {
+        //get id jika ada
+        $where  = isset($input['id']) ? "WHERE id=".$input['id'] : "";
+        $qry    = $conn->query("SELECT * FROM keranjang ".$where);
 
-    //jika data ditemukan
-    if ( $qry->num_rows > 0 ) {
-        echo json_encode($qry->fetch_all(MYSQLI_ASSOC));
-    }else {
-        echo "Data tidak ditemukan";
-    }
+        //jika data ditemukan
+        if ( $qry->num_rows > 0 ) {
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= $qry->fetch_all(MYSQLI_ASSOC);
+        }else {
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "Data tidak ditemukan";
+        }
 
-}else if ($method == 'POST') {
+    }else if ($method == 'POST') {
 
-    //cek param
-    if ( !isset($input['id_produk']) ){
-        echo "Produk tidak boleh kosong";
-    }else if ( !isset($input['id_member']) ){
-        echo "ID Member tidak boleh kosong";
-    }else if ( !isset($input['qty']) ){
-        echo "Qty tidak boleh kosong";
-    }else if ( !isset($input['price']) ){
-        echo "Harga tidak boleh kosong";
-    }else if ( !isset($input['total']) ){
-        echo "Total tidak boleh kosong";
-    }else{
-        //cek jika data yg dimasukkan banyak
-        $count = is_array($input['id_produk']) ? count($input['id_produk']) : 1;
+        //cek param
+        if ( !isset($input['id_produk']) ){
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "Produk tidak boleh kosong";
+        }else if ( !isset($input['id_member']) ){
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "ID Member tidak boleh kosong";
+        }else if ( !isset($input['qty']) ){
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "Qty tidak boleh kosong";
+        }else if ( !isset($input['price']) ){
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "Harga tidak boleh kosong";
+        }else if ( !isset($input['total']) ){
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "Total tidak boleh kosong";
+        }else{
+            //cek jika data yg dimasukkan banyak
+            $count = is_array($input['id_produk']) ? count($input['id_produk']) : 1;
+            
+            if ( $count > 1) {
+                $stmt = $conn->stmt_init();
+
+                if ( $stmt->prepare("INSERT INTO keranjang (id_produk, id_member, qty, price, percent_discount, total, created_date, updated_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)") ){
+                    
+                    // prepare and bind
+                    $stmt -> bind_param("ssssssss", $id_produk, $id_member, $qty, $price, $percent, $total, $created, $updated);
+
+                    for ($i=0; $i < $count ; $i++) {     
+                        // set parameters and execute
+                        $id_produk   = $input['id_produk'][$i];
+                        $id_member   = $input['id_member'];
+                        $qty         = $input['qty'][$i];
+                        $price       = $input['price'][$i];
+                        $percent     = isset($input['percent'][$i]) ? $input['percent'][$i]: 0;
+                        $total       = $input['total'][$i];
+                        $created     = date('Y-m-d H:i:s');
+                        $updated     = date('Y-m-d H:i:s');
+                        $stmt->execute();
         
-        if ( $count > 1) {
-            $stmt = $conn->stmt_init();
+                    }
+                }
+                
+                $_SESSION['result'] = FALSE;
+                $_SESSION['message']= "Data berhasil disimpan";
 
-            if ( $stmt->prepare("INSERT INTO keranjang (id_produk, id_member, qty, price, percent_discount, total, created_date, updated_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)") ){
+            }else {
                 
                 // prepare and bind
+                $stmt = $conn->prepare("INSERT INTO keranjang (id_produk, id_member, qty, price, percent_discount, total, created_date, updated_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
                 $stmt -> bind_param("ssssssss", $id_produk, $id_member, $qty, $price, $percent, $total, $created, $updated);
 
-                for ($i=0; $i < $count ; $i++) {     
-                    // set parameters and execute
-                    $id_produk   = $input['id_produk'][$i];
-                    $id_member   = $input['id_member'];
-                    $qty         = $input['qty'][$i];
-                    $price       = $input['price'][$i];
-                    $percent     = isset($input['percent'][$i]) ? $input['percent'][$i]: 0;
-                    $total       = $input['total'][$i];
-                    $created     = date('Y-m-d H:i:s');
-                    $updated     = date('Y-m-d H:i:s');
-                    $stmt->execute();
-    
-                }
+                // set parameters and execute
+                $id_produk   = $input['id_produk'];
+                $id_member   = $input['id_member'];
+                $qty         = $input['qty'];
+                $price       = $input['price'];
+                $percent     = isset($input['percent']) ? $input['percent'] : 0;
+                $total       = $input['total'];
+                $created     = date('Y-m-d H:i:s');
+                $updated     = date('Y-m-d H:i:s');
+                $stmt->execute();
+
+                $_SESSION['result'] = FALSE;
+                $_SESSION['message']= "Data berhasil disimpan";
             }
-            
-
-            echo "Data berhasil disimpan";
-
-        }else {
-            
+        }
+    }else if ($method == 'PUT') {
+        //cek param
+        if ( !isset($input['id']) ){
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "ID tidak boleh kosong";
+        }else if ( !isset($input['id_produk']) ){
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "Produk tidak boleh kosong";
+        }else if ( !isset($input['id_member']) ){
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "ID Member tidak boleh kosong";
+        }else if ( !isset($input['qty']) ){
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "Qty tidak boleh kosong";
+        }else if ( !isset($input['price']) ){
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "Harga tidak boleh kosong";
+        }else{
             // prepare and bind
-            $stmt = $conn->prepare("INSERT INTO keranjang (id_produk, id_member, qty, price, percent_discount, total, created_date, updated_date) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-            $stmt -> bind_param("ssssssss", $id_produk, $id_member, $qty, $price, $percent, $total, $created, $updated);
+            $stmt = $conn->prepare("UPDATE keranjang SET id_produk = ?, id_member = ?, qty = ?, price = ?, percent_discount = ?, total = ?, updated_date = ?  WHERE id = ?");
+            $stmt -> bind_param("ssssssss", $id_produk, $id_member, $qty, $price, $percent, $total, $updated, $id);
 
             // set parameters and execute
             $id_produk   = $input['id_produk'];
@@ -74,55 +118,34 @@ if ($method == "GET") {
             $price       = $input['price'];
             $percent     = isset($input['percent']) ? $input['percent'] : 0;
             $total       = $input['total'];
-            $created     = date('Y-m-d H:i:s');
             $updated     = date('Y-m-d H:i:s');
+            $id          = $input['id'];
             $stmt->execute();
 
-            echo "Data berhasil disimpan";
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "Data berhasil diupdate";
+
         }
-    }
-}else if ($method == 'PUT') {
-    //cek param
-    if ( !isset($input['id']) ){
-        echo "ID tidak boleh kosong";
-    }else if ( !isset($input['id_produk']) ){
-        echo "Produk tidak boleh kosong";
-    }else if ( !isset($input['id_member']) ){
-        echo "ID Member tidak boleh kosong";
-    }else if ( !isset($input['qty']) ){
-        echo "Qty tidak boleh kosong";
-    }else if ( !isset($input['price']) ){
-        echo "Harga tidak boleh kosong";
-    }else{
-        // prepare and bind
-        $stmt = $conn->prepare("UPDATE keranjang SET id_produk = ?, id_member = ?, qty = ?, price = ?, percent_discount = ?, total = ?, updated_date = ?  WHERE id = ?");
-        $stmt -> bind_param("ssssssss", $id_produk, $id_member, $qty, $price, $percent, $total, $updated, $id);
 
-        // set parameters and execute
-        $id_produk   = $input['id_produk'];
-        $id_member   = $input['id_member'];
-        $qty         = $input['qty'];
-        $price       = $input['price'];
-        $percent     = isset($input['percent']) ? $input['percent'] : 0;
-        $total       = $input['total'];
-        $updated     = date('Y-m-d H:i:s');
-        $id          = $input['id'];
-        $stmt->execute();
+    }else if ($method == "DELETE"){
 
-        echo "Data berhasil diupdate";
+        if (!isset($input['id'])) {
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "id tidak boleh kosong";
+        }else {
+            $id  = $input['id'];
+            $qry = $conn->query("DELETE FROM keranjang WHERE id = ".$id);
 
-    }
-
-}else if ($method == "DELETE"){
-
-    if (!isset($input['id'])) {
-        echo "id tidak boleh kosong";
+            $_SESSION['result'] = FALSE;
+            $_SESSION['message']= "Data berhasil di delete";
+        }
     }else {
-        $id  = $input['id'];
-        $qry = $conn->query("DELETE FROM keranjang WHERE id = ".$id);
-        echo "Data berhasil di delete";
+        $_SESSION['result'] = FALSE;
+        $_SESSION['message']= "Method tidak di dukung!";
     }
 }else {
-    echo "Method tidak di dukung!";
+    $_SESSION['result'] = FALSE;
+    $_SESSION['message']= "Anda belum login";    
 }
+echo json_encode($_SESSION);
 ?>
